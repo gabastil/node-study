65347273aa7d115bfb9013981989729240fc2cb9
gabastil <glenn.abastillas@gmail.com>
2020-04-29 18:19:48 -0400

    Updated https.js to include variable string names

--- js/http.js
+++ js/http.js
@@ -9,7 +9,9 @@ http.createServer(function (reader, response){
 	response.writeHead(200, {'Content-Type' : 'text/plain'});
 
 	// Send the response body as "Hello World"
-	response.end('Hello World\n');
+    var second_line = "this is my name";
+    var message = `Hello World\nMy name is ${second_line}`;
+    response.end(message);
 }).listen(8081);
 
 console.log("Server set up on http://127.0.0.1:8081");
