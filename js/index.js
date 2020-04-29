447e415c1bad022fca34d71de37c2151486c5a47
gabastil <glenn.abastillas@gmail.com>
2020-04-29 16:56:15 -0400

    First commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/**
+ * filename : index.js
+ * author : glenn abastillas
+ * created : 4/29/2020
+ * description : This file is used for the node.js study
+ */
+require('jquery');
+
+console.log("Hello World")
+
+$.getJSON();
